7b46ff99549c2ea908b979d67f632c47dc8cddb3
Sergio Sanz Cebollero <scebollero@guidewire.com>
2020-11-23 20:31:04 +0100

    Stage 2/6 Completed

--- search/engine/SimpleSearchEngine.java
+++ search/engine/SimpleSearchEngine.java
@@ -1,13 +1,34 @@
 package search.engine;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class SimpleSearchEngine {
-    public static int findIndex(String[] words, String searchKey) {
+    public static int findIndex(String[] records, String searchKey) {
         int index = -1;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].equals(searchKey)) {
-                index = i + 1;
+        var lowerCasedSearchKey = searchKey.toLowerCase();
+
+        for (int i = 0; i < records.length; i++) {
+            var record = records[i].toLowerCase();
+            if (record.contains(lowerCasedSearchKey)) {
+                index = i;
             }
         }
+
         return index;
+    }
+
+    public static String[] findCoincidences(String[] records, String searchKey) {
+        var foundRecords = new String[records.length];
+
+        for (int i = 0, j = 0; i < records.length; i++) {
+            var index = findIndex(new String[]{records[i]}, searchKey);
+            if (index != - 1) {
+                foundRecords[j] = records[i];
+                j++;
+            }
+        }
+
+        return Arrays.stream(foundRecords).filter(Objects::nonNull).toArray(String[]::new);
     }
 }
